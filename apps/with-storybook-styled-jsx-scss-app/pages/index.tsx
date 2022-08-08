c9fe81b45f26a1cd576bb0978ca4a52275f3fc31
Josh Baker <josh.jacob.baker@gmail.com>
2022-08-08 12:08:08 -0700

    apps/storybook/package.json/script added specific port to prevent nextjs from running on a port already in use; packages/ui/components/footer added tailwindcss styling

--- apps/with-storybook-styled-jsx-scss-app/pages/index.tsx
+++ apps/with-storybook-styled-jsx-scss-app/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
-          Powered by <Image src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by <Image src="/vercel.svg" width={72} height={16} alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
--- packages/ui/components/Footer.tsx
+++ packages/ui/components/Footer.tsx
@@ -1,7 +1,42 @@
 import React from "react"
 
 function Footer() {
-  return <footer className="font-bold text-blue-700">Footer</footer>
+  return (
+    <footer className="flex  justify-between items-center h-20 font-bold text-blue-700 bg-blue-400">
+      <h6
+        className="text-3xl ml-32 text-4xl cursor-pointer focus:bottom-full transition ease-in-out 
+        hover:delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 active:duration-75 select-none active:translate-y-0">
+        Footer
+      </h6>
+
+      <ul className="flex justify-between items-center w-1/4 m-4 bg-slate-300 mr-6">
+        <li>
+          <a
+            className="cursor-pointer focus:bottom-full transition 
+          ease-in-out hover:delay-150 hover:-translate-y-1 hover:scale-110 
+          hover:duration-300 active:duration-75 select-none active:translate-y-0">
+            1 Item
+          </a>
+        </li>
+        <li>
+          <a
+            className="cursor-pointer focus:bottom-full transition 
+          ease-in-out hover:delay-150 hover:-translate-y-1 hover:scale-110 
+          hover:duration-300 active:duration-75 select-none active:translate-y-0">
+            1 Item
+          </a>
+        </li>
+        <li>
+          <a
+            className="cursor-pointer focus:bottom-full transition 
+          ease-in-out hover:delay-150 hover:-translate-y-1 hover:scale-110 
+          hover:duration-300 active:duration-75 select-none active:translate-y-0">
+            1 Item
+          </a>
+        </li>
+      </ul>
+    </footer>
+  )
 }
 
 export default Footer
