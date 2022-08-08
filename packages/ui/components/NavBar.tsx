86ced5f954850309d687b8f7d557c4e542a44049
Josh Baker <josh.jacob.baker@gmail.com>
2022-08-08 09:29:03 -0700

    packages/ui components/navbar/menu animation in tailwindcss

--- packages/ui/components/NavBar.tsx
+++ packages/ui/components/NavBar.tsx
@@ -4,10 +4,10 @@ function NavBar() {
   return (
     <nav className="flex top-0 h-20 w-full text-2xl text-slate-800 bg-blue-400 justify-between">
       <div>NavBar</div>
-      <div className="flex flex-col justify-around w-24 mr-4 mt-4">
-        <div className="w-full border-t-8"></div>
-        <div className="w-full border-t-8"></div>
-        <div className="h-1/3 w-full border-t-8"></div>
+      <div className="group transition-all ease-in-out delay-150 flex flex-col justify-around w-12 mr-4 mt-4 mb-4">
+        <div className="group-hover:w-4 transition-all ease-in-out delay-75 duration-100 w-full border-t-8 rounded-lg"></div>
+        <div className="group-hover:w-4 transition-all ease-in-out delay-100 duration-200 w-full border-t-8 rounded"></div>
+        <div className="group-hover:w-4 transition-all ease-in-out delay-150 duration-300 w-full border-t-8 rounded"></div>
       </div>
     </nav>
   )
