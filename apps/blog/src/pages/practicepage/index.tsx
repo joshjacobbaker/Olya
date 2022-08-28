c83747ecfd8e16c3c2c98b2409c8e94e4bea431d
Josh Baker <josh.jacob.baker@gmail.com>
2022-08-27 19:26:59 -0700

    apps/blog practicePage serverSideProps dynamic tailwindCSS

--- apps/blog/src/pages/practicepage/index.tsx
+++ apps/blog/src/pages/practicepage/index.tsx
@@ -5,10 +5,10 @@ import Link from "next/link"
 import { PracticeLayout } from "ui"
 import { useQuery } from "@tanstack/react-query"
 
-function Page() {
+function Page({ data }) {
   const query = useQuery(["count"], () => [1, 2, 3])
 
-  console.log(query)
+  console.log(`props: ${JSON.stringify(data.name)}`)
 
   return (
     <div>
@@ -16,8 +16,14 @@ function Page() {
         <title>practicepage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>practicepage</p>
-      {query.data && query.data.map((d) => <p>{d}</p>)}
+
+      <h1>Practice Page</h1>
+
+      <p className="border-b bg-green-200 rounded px-4 py-2 my-2">Server Side Request is returning the name of: {data.name && <span className="text-lg font-bold text-red-500">{data.name}</span>}</p>
+
+      <p>React Query:</p>
+
+      <ul>{query.data && query.data.map((d, i) => <li className={`ml-2 ${i % 2 === 0 ? "ml-8" : ""}`}>{d}</li>)}</ul>
       <div className="grid grid-flow-col gap-4">
         <Link href="/" className="bg-red-200 py-4 px-8">
           <a className="bg-blue-900 py-4 px-8 flex justify-center items-center rounded-lg">Home Page?</a>
@@ -28,6 +34,16 @@ function Page() {
       </div>
     </div>
   )
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`http://localhost:3000/api/hello`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
 
 Page.PageLayout = PracticeLayout
