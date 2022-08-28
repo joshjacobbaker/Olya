cafdc22a6659878dc4fb60d75111a85de4ca013b
Josh Baker <josh.jacob.baker@gmail.com>
2022-08-27 17:09:29 -0700

    apps/blog modal prevented bubbling propogation with e.stopPropogation() for intended functionality

--- apps/blog/src/components/ModalFromScratch.tsx
+++ apps/blog/src/components/ModalFromScratch.tsx
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { useCallback } from "react"
 
 type Props = {
   modalIsOpen: boolean
   closeModal: () => void
   contentLabel: string
 }
+
 export default function Modal(props: Props) {
   // const [showModal, setShowModal] = React.useState(false)
   const { modalIsOpen, closeModal, contentLabel } = props
+
+  const onClickHandlerStopPropogation = (e) => {
+    e.stopPropagation()
+    closeModal()
+  }
+
   return (
     <div onClick={closeModal}>
       {modalIsOpen ? (
@@ -19,7 +26,7 @@ export default function Modal(props: Props) {
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">Modal Title</h3>
-                  <button className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={closeModal}>
+                  <button className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={onClickHandlerStopPropogation}>
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">×</span>
                   </button>
                 </div>
@@ -29,10 +36,10 @@ export default function Modal(props: Props) {
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={closeModal}>
+                  <button className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={onClickHandlerStopPropogation}>
                     Close
                   </button>
-                  <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={closeModal}>
+                  <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={onClickHandlerStopPropogation}>
                     Save Changes
                   </button>
                 </div>
