a29f07db0e948c88c6536c40c4d0902c17341dc7
Josh Baker <josh.jacob.baker@gmail.com>
2022-09-26 21:04:07 -0700

    app/blogs connected mongo to nextAuth

--- apps/blog/src/database/connectMongo.ts
+++ apps/blog/src/database/connectMongo.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose"
-const MONGO_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@nextjscluster.brka1hs.mongodb.net/?retryWrites=true&w=majority`
+// const MONGO_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@nextjscluster.brka1hs.mongodb.net/?retryWrites=true&w=majority`
+
 const connectMongo = async () => {
   // console.log(`${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}`)
 
-  return mongoose.connect(MONGO_URI)
+  return mongoose.connect(process.env.MONGO_URI)
 }
 
 export default connectMongo
