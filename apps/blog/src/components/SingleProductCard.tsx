a8ec1336ef246be52f645682b5361418dbfbc3ab
Josh Baker <josh.jacob.baker@gmail.com>
2022-10-02 21:53:37 -0700

    apps/blog -- still working on shoppingCartContext

--- apps/blog/src/components/SingleProductCard.tsx
+++ apps/blog/src/components/SingleProductCard.tsx
@@ -1,27 +1,35 @@
 import React from "react"
 import Image from "next/image"
 
+// interface UiReducerStateProps {
+//   id: number
+//   productName: string
+//   price: number
+//   image: string
+//   inStock: number
+//   fastDelivery: boolean
+//   ratings: number
+// }
+
 interface UiReducerStateProps {
   id: number
-  productName: string
+  name: string
   price: number
-  image: string
-  inStock: number
-  fastDelivery: boolean
-  ratings: number
 }
 
-const SingleProductCard = ({ id, productName, price, image, inStock, fastDelivery, ratings }: UiReducerStateProps) => {
+// const SingleProductCard = ({ id, productName, price, image, inStock, fastDelivery, ratings }: UiReducerStateProps) => {
+const SingleProductCard = ({ id, name, price }: UiReducerStateProps) => {
   return (
     <li>
       <ul>
         <li>id: {id}</li>
-        <li>productName: {productName}</li>
+        {/* <li>productName: {productName}</li> */}
+        <li>name: {name}</li>
         <li>price: {price}</li>
         <li>{/* <Image src={image} width={300} height={300} /> */}</li>
-        <li>inStock: {inStock}</li>
-        <li>fastDelivery: {fastDelivery}</li>
-        <li>ratings: {ratings}</li>
+        {/* <li>inStock: {inStock}</li> */}
+        {/* <li>fastDelivery: {fastDelivery}</li> */}
+        {/* <li>ratings: {ratings}</li> */}
       </ul>
     </li>
   )
